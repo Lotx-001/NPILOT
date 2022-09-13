f5aeeab82d4cba257ae3373e717913d9feb021d0
Lotx-001 <72420846+Lotx-001@users.noreply.github.com>
2022-09-13 16:12:32 +0300

    Fix

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -62,9 +62,9 @@ class CarInterface(CarInterfaceBase):
     elif lateral_control == 'LQR':
       ret.lateralTuning.init('lqr')
 
-      ret.lateralTuning.lqr.scale = 1600.
-      ret.lateralTuning.lqr.ki = 0.01
-      ret.lateralTuning.lqr.dcGain = 0.0025
+      ret.lateralTuning.lqr.scale = 1800.0
+      ret.lateralTuning.lqr.ki = 0.12
+      ret.lateralTuning.lqr.dcGain = 0.0285
 
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
