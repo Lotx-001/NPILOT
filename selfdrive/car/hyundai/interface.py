cda49e9f2722c50ac2b1ae8b6fcd0cd0295bf0c6
Lotx-001 <72420846+Lotx-001@users.noreply.github.com>
2022-09-14 20:07:18 +0300

    Fix

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -62,15 +62,15 @@ class CarInterface(CarInterfaceBase):
     elif lateral_control == 'LQR':
       ret.lateralTuning.init('lqr')
 
-      ret.lateralTuning.lqr.scale = 1800.0
-      ret.lateralTuning.lqr.ki = 0.12
-      ret.lateralTuning.lqr.dcGain = 0.0285
+      ret.lateralTuning.lqr.scale = 1750.0
+      ret.lateralTuning.lqr.ki = 0.3
+      ret.lateralTuning.lqr.dcGain = 0.003
 
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
-      ret.lateralTuning.lqr.k = [-110., 451.]
-      ret.lateralTuning.lqr.l = [0.33, 0.318]
+      ret.lateralTuning.lqr.k = [-100., 450.]
+      ret.lateralTuning.lqr.l = [0.22, 0.318]
     else:
       ret.lateralTuning.init('torque')
       ret.lateralTuning.torque.useSteeringAngle = True
@@ -84,9 +84,9 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.torque.deadzone = 0.0
 
 
-    ret.steerRatio = 1800.0
+    ret.steerRatio = 1750.0
     ret.steerActuatorDelay = 0.1
-    ret.steerRateCost = 0.41
+    ret.steerRateCost = 0.5
 
     ret.steerLimitTimer = 1.5
 
